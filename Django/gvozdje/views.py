34621bb35f3dc5c88f964cebad9b1aa2716feb31
Marec99 <radisicm99@gmail.com>
2020-12-18 01:35:43 +0100

    Sitne dorade, problem sa save metodom, MulktiValueDickiEror

--- Django/gvozdje/views.py
+++ Django/gvozdje/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 
 
-from Django.gvozdje.models import Mreza
+from .models import Mreza
 
 
 def gvozdje(request):
     return render(request, 'home.html')
 
 def mrezaIzmene(request):
-    if request == "POST":
-        naziv = request.POST['Naziv']
-        dimenzijeKocke = request.POST['DimenzijeKocke']
-        dimenzije = request.POST['Dimenzije']
-        jedinicaMere = request.POST['JedinicaMere']
+    if request.method == "POST":
+
+        naziv = request.POST['naziv']
+        dimenzijeKocke = request.POST['kocka']
+        dimenzije = request.POST['dimenzije']
+        jedinicaMere = request.POST['jedinicaMere']
         cena_DIN = request.POST['cena']
         proizvedeno_Od = request.POST['gvozdje']
         inc = Mreza(naziv=naziv, dimenzije=dimenzije, dimenzijeKocke=dimenzijeKocke, jedinicaMere=jedinicaMere, cena_DIN=cena_DIN,proizvedeno_Od=proizvedeno_Od)
         inc.save()
+        print("Mreza je dodata!!!")
     return render(request, 'mrezaIzmene.html')
 
 def gvozdjeIzmene(request):
