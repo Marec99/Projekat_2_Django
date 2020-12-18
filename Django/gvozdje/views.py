ba01d14d1c769f796a9d890e55ec05be115994ff
Marec99 <radisicm99@gmail.com>
2020-12-18 01:04:10 +0100

    ...

--- Django/gvozdje/views.py
+++ Django/gvozdje/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+from Django.gvozdje.models import Mreza
 
 
 def gvozdje(request):
     return render(request, 'home.html')
 
 def mrezaIzmene(request):
+    if request == "POST":
+        naziv = request.POST['Naziv']
+        dimenzijeKocke = request.POST['DimenzijeKocke']
+        dimenzije = request.POST['Dimenzije']
+        jedinicaMere = request.POST['JedinicaMere']
+        cena_DIN = request.POST['cena']
+        proizvedeno_Od = request.POST['gvozdje']
+        inc = Mreza(naziv=naziv, dimenzije=dimenzije, dimenzijeKocke=dimenzijeKocke, jedinicaMere=jedinicaMere, cena_DIN=cena_DIN,proizvedeno_Od=proizvedeno_Od)
+        inc.save()
     return render(request, 'mrezaIzmene.html')
 
 def gvozdjeIzmene(request):
